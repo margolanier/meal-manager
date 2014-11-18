3baf70d21494d3dd4680d91913fce1e900fdfc00
margolanier <mplanier@live.unc.edu>
2014-11-18 10:49:27 -0500

    'add recipe to day' modal

--- js/firebase.js
+++ js/firebase.js
@@ -13,6 +13,8 @@ $( document ).ready(function() {
     
     //function to populate menu
     loadRecipes();
+    
+    addToDOW();
 });
 
 
@@ -68,7 +70,60 @@ function loadRecipes(){
     
 }
 
-
+function addToDOW(){
+    var onClick = false;
+    varMon = 0;
+    varTues = 0;
+    varWed = 0;
+    varThurs = 0;
+    varFri = 0;
+    
+    //make new Firebase list? for each user?
+    var recipeListRef = new Firebase('https://my-cookbook.firebaseio.com///recipes');
+    
+    recipeListRef.once('value', function(addToDOW) {
+	// store dataSnapshot for use in below examples.
+	varMon = onClick.val();
+	console.log(varMon);
+	//length of an object.
+	//console.log(Object.keys(recipeListView).length)
+	var listItems = "";
+	var list = "";
+	
+	
+	//idk.....
+	$.each(varMon, function(key, val) {
+	    var recipes = [];
+	    
+	    //console.log('Key: ' + key + '  Val: ' + val)
+	    recipes.push(key);
+	    $.each(val, function(key, val) {
+		recipes.push(val); 
+	    });
+	    
+	    
+	    listItems += '<li><img src=" '+ varMon[3] +' ">'
+	    listItems += '<h2>' + varMon[0] + '</h2>';
+	    listItems += '<p><strong>Ready in: </strong>' + varMon[4] + '</p>';
+	    listItems += '<p><strong>Serving Size: </strong>' + varMon[5] + '</p>';
+	    listItems += '<p><strong>Ingredients: </strong><br>' + varMon[2] + '</p>';
+	    listItems += '<p><strong>Directions: </strong><br>' + varMon[1] + '</p></li>'
+            
+	    
+	    list += '<li><a href="#"><img src=" '+ varMon[3] +' ">'
+	    list += '<h2>' + varMon[0] + '</h2>';
+	    list += '<p><strong>Ready in: </strong>' + varMon[4] + '</p>';
+	    list += '<a href="#purchase" data-rel="popup" data-position-to="window" data-transition="pop">Purchase album</a></a></li>'
+	    
+	});
+	
+	console.log(list);
+	$("#monMenu").html(list);
+	$("#monMenu").listview("refresh");
+	
+    });
+    
+}
 
 
 
